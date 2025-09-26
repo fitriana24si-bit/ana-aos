2c5a6d67729f976323755740b6ec67ad3cc23f7e
anapcr <fitriana24si@mahasiswa.pcr.ac.id>
2025-09-26 10:37:37 +0700

    Latihan Passing Data dan Git

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,8 +3,92 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PegawaiController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+ // Data tanggal lahir dan tanggal masuk
+        $tanggalLahir = new \DateTime('2006-08-05');
+        $tanggalMasuk = new \DateTime('2022-01-10');
+        $hariIni = new \DateTime();
+
+        // Menghitung umur secara manual
+        $umur = $hariIni->diff($tanggalLahir)->y;
+
+        // Menghitung selisih hari secara manual
+        $sejakBerapaHari = $hariIni->diff($tanggalMasuk)->days;
+
+        // Data pegawai yang sudah diperbarui
+        $data_pegawai = [
+            'nama' => 'Fitriana Tasya',
+            'tanggal_lahir' => '17 Agustus 2005',
+            'umur' => $umur,
+            'hobi' => ['Memasak','Membaca','Bermain musik','Traveling','Menulis'],
+            'tanggal_masuk' => '10 Januari 2022',
+            'sejak_berapa_hari' => $sejakBerapaHari,
+            'semester' => 3,
+            'status' => 'Mahasiswa',
+        ];
+
+        // Meneruskan data ke view
+        return view('home-pegawai', ['pegawai' => $data_pegawai]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $param1)
+    {
+      // if($param1 == "detail"){
+      //  return view('halaman-mahasiswa-detail');
+
+       //}else if ($param1 == "profile"){
+        //return view('halaman-mahasiswa-profile');
+       //}
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\MahasiswaController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -37,3 +40,8 @@ Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 Route::get('/about', function () {
     return view('halaman-about');
 });
+
+Route::get('/home', [HomeController::class,'index']
+);
+
+Route::get('/pegawai', [PegawaiController::class, 'index']);
