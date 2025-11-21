9f308dd3394f4b08d9c58832e103b44534580370
fitriana24si-bit <fitriana24si@mahasiswa.pcr.ac.id>
2025-11-21 07:46:18 +0700

    filter

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -9,9 +9,10 @@ class PelangganController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data['dataPelanggan'] = Pelanggan::paginate(10);
+        $filterableColumns = ['gender'];
+        $data['dataPelanggan'] = Pelanggan::filter($request, $filterableColumns )->paginate(10);
         return view('admin.pelanggan.index', $data);
     }
 
